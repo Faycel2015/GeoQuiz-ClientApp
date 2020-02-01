aae77bf89b0a3252afb98690d1dcc4d7d8c17b96
Romain Guillot <rguillot42@gmail.com>
2020-02-01 17:43:39 -0500

    Format menu widget

--- lib/ui/widgets/app_menu.dart
+++ lib/ui/widgets/app_menu.dart
@@ -9,9 +9,21 @@ class AppMenu extends StatelessWidget {
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
-          ListTile(leading: Icon(Icons.local_drink), title: Text("Buy me a coffee"), onTap: () {},),
-          ListTile(leading: Icon(Icons.delete_forever), title: Text("Reset my data"), onTap: () {},),
-          ListTile(leading: Icon(Icons.bug_report), title: Text("Report a bug"), onTap: () {},),
+          ListTile(
+            leading: Icon(Icons.local_drink), 
+            title: Text("Buy me a coffee"), 
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.delete_forever), 
+            title: Text("Reset my data"), 
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.bug_report), 
+            title: Text("Report a bug"), 
+            onTap: () {},
+          ),
         ],
       );
   }
