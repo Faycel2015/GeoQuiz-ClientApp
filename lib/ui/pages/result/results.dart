455100624bbdc6d7959e83f51491dd56b97b5714
Romain Guillot <rguillot42@gmail.com>
2020-03-16 15:19:46 -0400

    Add widget in result page to show local progressions

--- lib/ui/pages/result/results.dart
+++ lib/ui/pages/result/results.dart
@@ -1,11 +1,15 @@
+import 'package:app/logic/progression_provider.dart';
 import 'package:app/logic/quiz_provider.dart';
+import 'package:app/models/progression.dart';
 import 'package:app/ui/pages/home/homepage.dart';
 import 'package:app/ui/pages/quiz/quiz.dart';
 import 'package:app/ui/shared/dimens.dart';
 import 'package:app/ui/widgets/button.dart';
 import 'package:app/ui/widgets/flex_spacer.dart';
+import 'package:app/ui/widgets/progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 
@@ -20,6 +24,12 @@ class ResultsPage extends StatelessWidget {
           Text("Results", style: Theme.of(context).textTheme.headline1,),
           FlexSpacer(),
           Center(child: QuizScore()),
+          FlexSpacer.big(),
+          Consumer<LocalProgressionProvider>(
+            builder: (_, localProgression, __) => LocalProgressionsList(
+              progressions: localProgression.progressions.values.toList(),
+            ),
+          ),
           Expanded(child: Container()),
           Center(child: ResultsButtonList())
         ],
@@ -72,5 +82,53 @@ class ResultsButtonList extends StatelessWidget {
     Navigator.of(context).pushReplacement(MaterialPageRoute(
       builder: (context) => page
     ));
+  }
+}
+
+
+class LocalProgressionsList extends StatelessWidget {
+
+  final List<QuizThemeProgression> progressions;
+
+  LocalProgressionsList({@required this.progressions}) {
+    // progressions.sort();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: Dimens.borderRadius
+      ),
+      padding: EdgeInsets.all(Dimens.surfacePadding),
+      child: LayoutBuilder(
+        builder: (_, constraints) => Wrap(
+            spacing: Dimens.normalSpacing,
+            direction: Axis.vertical,
+            children: progressions.map((p) => 
+              SizedBox(
+                width: constraints.maxWidth,
+                child: Row(
+                  children: <Widget>[
+                    SvgPicture.string(
+                      p.theme.icon, 
+                      height: 35, 
+                      color: Color(p.theme.color)
+                    ),
+                    FlexSpacer.small(),
+                    Expanded(
+                      child: ProgressBar(
+                        percentage: p.percentage,
+                        color: Color(p.theme.color),
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ).toList(),
+          ),
+        ),
+    );
   }
 }
